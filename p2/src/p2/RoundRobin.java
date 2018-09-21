2b764fd9b292cfe59d0fc216f2ab0637bd321f2b
Pratyusha Pogaru <plpratyusha@gmail.com>
2018-09-21 16:46:02 -0700

    Refactored vars & methods, moved methods around

--- p2/src/p2/RoundRobin.java
+++ p2/src/p2/RoundRobin.java
@@ -2,178 +2,166 @@ package p2;
 
 import java.util.*;
 
-/**
-* Round Robin scheduling algorithm
-*/
 public class RoundRobin {
+
+	// Private variables for the RoundRobin algorithm
 	private final static int MAX_QUANTA_RUN_TIME = 100;
-	private final static int TIME_SLICE = 1;				//1 quantum
+	private final static int TIMESLICE = 1; // 1 quantum
 	private static int timeCounter;
-	
 	private Queue<Process> queue;
 	private ArrayList<Process> allProcesses;
-	
-	private String quantaScale;
 	private String processesInformation;
-	
+	private String quantaScale;
+
 	/**
-	 * Constructor RR takes an incoming processes and sorts based on arrival time. 
-	 * @param allProcesses - a list of randomly generated processes.
+	 * Constructor for RoundRobin takes incoming processes as input and sorts based on their arrival time
+	 * @param allProcesses - list of randomly generated processes
 	 */
 	public RoundRobin(ArrayList<Process> someProcesses) {
 		processesInformation = "";
 		quantaScale = "";
 		allProcesses = someProcesses;
-//		Collections.sort(this.allProcesses, new Comparator<Process>() {
-//			public int compare(Process o1, Process o2) {
-//				return o1.getArrivalTime() < o2.getArrivalTime() ? -1 :
-//					 o1.getArrivalTime() > o2.getArrivalTime() ? 1: 0;
-//			}
-//		});
-		
+
 		Collections.sort(this.allProcesses, new Comparator<Process>() {
 			public int compare(Process o1, Process o2) {
 				if (o1.getArrivalTime() < o2.getArrivalTime()) {
 					return -1;
-				}
-				else if (o1.getArrivalTime() > o2.getArrivalTime()) {
+				} else if (o1.getArrivalTime() > o2.getArrivalTime()) {
 					return 1;
-				}
-				else return 0;
+				} else
+					return 0;
 			}
 		});
-		
+
 		queue = new LinkedList<Process>();
-		for(Process proc: this.allProcesses){
-			
+		for (Process proc : this.allProcesses) {
+
 			queue.add(proc);
 			this.processesInformation += this.printProcessesInfo(proc);
 		}
 	}
-	
+
 	/**
-	 * Runs Round Robin on list of processes and logs the information
+	 * Runs the algorithm on a list of processes; outputs info about run
 	 */
 	public void run() {
 		timeCounter = 0;
 		Process current = null;
-		boolean starved = false; 
-		boolean finish = false;
-		Queue<Process> readyQueue = new LinkedList<Process>();
-		while (!finish) {
-			// We use starve to kill any inactive processes in readyQueue and the boolean acts as a checker
-			starved = checkStarve(allProcesses,readyQueue,timeCounter, MAX_QUANTA_RUN_TIME, starved);
-			// If starved, we do not add the processes from queue to readyQueue because the current time is already over 100 quanta
-			if(!starved) {
-				// We are using a loop instead of poll because there can be more than 1 processes those have arrived i.e. 1.2, 1.3. 1.5 have arrived when current time is 2 
-				for(Process process: queue) {
-					if(process.getArrivalTime() <= timeCounter) {
-						readyQueue.add(process);
+		boolean starved = false; // Starve kills the inactive processes in the ready Queue
+		boolean finished = false; // The Boolean makes certain no process is being starved
+		Queue<Process> ready = new LinkedList<Process>();
+		while (!finished) {
+			starved = checkStarve(allProcesses, ready, timeCounter, MAX_QUANTA_RUN_TIME, starved);
+			if (!starved) { // If any process is starved, current time exceeded 100 quanta. Don't add any
+							// more processes from queue to ready
+				for (Process p : queue) {
+					if (p.getArrivalTime() <= timeCounter) {
+						ready.add(p);
 					} else {
 						break;
 					}
 				}
-				// We remove all processes from queue
-				queue.removeIf((process) -> (process.getArrivalTime() <= timeCounter));
+				queue.removeIf((process) -> (process.getArrivalTime() <= timeCounter)); // Empty the queue
 			}
 			if (current != null) {
 				if (current.getExpectedRunTime() > 0) {
-					readyQueue.add(current);
+					ready.add(current);
 				} else {
 					current.setFinishTime(timeCounter);
-					}
+				}
 				current = null;
 			}
-			
-			if((!starved && queue.isEmpty() && readyQueue.isEmpty() && current == null)|| 
-					(starved && readyQueue.isEmpty() && current == null)) {
-				finish = true;
+
+			if ((!starved && queue.isEmpty() && ready.isEmpty() && current == null)
+					|| (starved && ready.isEmpty() && current == null)) {
+				finished = true;
 			}
-			if (current == null && !readyQueue.isEmpty()) {
-				current = readyQueue.poll();
-				if(current.getStartTime() < 0) {
+			if (current == null && !ready.isEmpty()) {
+				current = ready.poll();
+				if (current.getStartTime() < 0) {
 					current.setStartTime(timeCounter);
 				}
-				current.setExpectedRunTime(current.getExpectedRunTime() - TIME_SLICE);
+				current.setExpectedRunTime(current.getExpectedRunTime() - TIMESLICE);
 				this.quantaScale += timeCounter + ": " + current.getProcessNumber() + "\n";
-			} else if (!finish) {
+			} else if (!finished) {
 				this.quantaScale += timeCounter + ": Waiting for a process\n";
 			}
-			if(!finish) 
-				timeCounter += TIME_SLICE;
+			if (!finished)
+				timeCounter += TIMESLICE;
 		}
 		current = null;
 		System.out.println(this.processesInformation);
 		System.out.println(this.quantaScale);
 	}
-	
+
 	/**
-	 * Calculates and logs the statistics of Round Robin
+	 * Removes processes that are starved, not started, from the ready Queue, and other processes; updates starved to true
+	 * 
+	 * @param processes - collection of processes
+	 * @param readyQueue - queue of ready processes
+	 * @param quanta - time when starved processes are ignored
+	 * @param currentTime - current quanta time
+	 * @param starved - check if a process is starved
+	 * @return true for starve if a purge occurs else false
 	 */
-	public void showCalculation() {
-		float totalTurnaroundTime = 0;
-		float totalWaitTime = 0;
-		float totalResponseTime = 0;
-		for(Process process: allProcesses) {
-			totalTurnaroundTime += process.getFinishTime() - process.getArrivalTime();
-			totalWaitTime += (process.getFinishTime() - process.getArrivalTime()) - process.getExpectedRunTimeForCal();
-			totalResponseTime += process.getStartTime() - process.getArrivalTime();
+	public boolean checkStarve(ArrayList<Process> processes, Queue<Process> readyQueue, int quanta, int currentTime, boolean starved) {
+		if (!starved && (currentTime >= quanta)) {
+			readyQueue.removeIf((process) -> (process.getStartTime() < 0));
+			processes.removeIf((process) -> (process.getStartTime() < 0));
+			starved = true;
 		}
-		float averageTurnaroundTime = 0;
-		float averageWaitTime = 0;
-		float averageResponseTime = 0;
+		return starved;
+	}
+
+	/**
+	 * Outputs statistics about the algorithm
+	 */
+	public void calculateStats() {
+		float turnAroundTime = 0;
+		float waitTime = 0;
+		float responseTime = 0;
+
+		for (Process pr : allProcesses) {
+			turnAroundTime += pr.getFinishTime() - pr.getArrivalTime();
+			waitTime += (pr.getFinishTime() - pr.getArrivalTime()) - pr.getExpectedRunTimeForCal();
+			responseTime += pr.getStartTime() - pr.getArrivalTime();
+		}
+		float avgTurnAroundTime = 0;
+		float avgResponseTime = 0;
+		float avgWaitTime = 0;
 		float throughput = 0;
-		
-		if(allProcesses.size() > 0) {
-			averageTurnaroundTime = totalTurnaroundTime / allProcesses.size();
-			averageWaitTime = totalWaitTime / allProcesses.size();
-			averageResponseTime = totalResponseTime / allProcesses.size();
-			if(timeCounter > 0)
-				throughput = (float)allProcesses.size() / timeCounter;
+
+		if (allProcesses.size() > 0) {
+			avgTurnAroundTime = turnAroundTime/allProcesses.size();
+			avgWaitTime = waitTime/allProcesses.size();
+			avgResponseTime = responseTime/allProcesses.size();
+
+			if (timeCounter > 0) {
+				throughput = (float) allProcesses.size() / timeCounter;
+			}
 		}
-		
-		System.out.println("Average turnaround time = " + averageTurnaroundTime);
-		System.out.println("Average waiting time = " + averageWaitTime);
-		System.out.println("Average response time = " + averageResponseTime);
+
+		System.out.println("Average turnaround time = " + avgTurnAroundTime);
+		System.out.println("Average waiting time = " + avgWaitTime);
+		System.out.println("Average response time = " + avgResponseTime);
 		System.out.println("Throughput = " + throughput);
 	}
-	
+
 	/**
-     * Removes starved processes, processes not started, from readyQueue and processes and sets starved to true
-     * @param processes - collection of processes
-     * @param readyQueue - ready processes queue
-     * @param currentTime - the current quanta time
-     * @param quanta - the time when starved processes are ignored
-     * @param starved - the boolean to check if starved 
-     * @return true for starve if a purge occurs else false
-     */
-	public boolean checkStarve(ArrayList<Process> processes, Queue<Process> readyQueue, int currentTime, int quanta, boolean starved) {
-		if(!starved && currentTime >= quanta) {
-			readyQueue.removeIf((process) -> (process.getStartTime() < 0));
-            processes.removeIf((process) -> (process.getStartTime() < 0));
-            starved = true;
-        }
-        return starved;
+	 * After running the algorithm, print information about the process that was run
+	 * 
+	 * @param currentProcess - the process to print information about
+	 * @return information on that process
+	 */
+	private String printProcessesInfo(Process currentProcess) {
+		int processNumber = currentProcess.getProcessNumber();
+		float arrivalTime = currentProcess.getArrivalTime();
+		float runTime = currentProcess.getExpectedRunTime();
+		int priority = currentProcess.getPriority();
+
+		String rrInfo = "\n" + "Process " + processNumber + " \n" + "Arrival Time of this process is: " + arrivalTime
+				+ " \n" + "Expected Run Time is: " + runTime + " \n" + "Priority " + priority + "\n";
+		return rrInfo;
 	}
-	
-	 /**
-     * After running the algorithm, print information about the process that was run. 
-     * @param currentProcess - the process to print information about 
-     * @return information on that process
-     */
-    private String printProcessesInfo(Process currentProcess) 
-    {
-        int processNumber = currentProcess.getProcessNumber(); 
-        float arrivalTime = currentProcess.getArrivalTime();
-        float runTime = currentProcess.getExpectedRunTime();
-        int priority = currentProcess.getPriority();
-        
-        String processInfo = "\n" + "Process " + processNumber +
-                " \n" + "Arrival Time of this process is: " + arrivalTime +
-                " \n" + "Expected Run Time is: " + runTime +
-                " \n" + "Priority " + priority + "\n";
-        
-    		return processInfo;
-    }
-	
+
 }
